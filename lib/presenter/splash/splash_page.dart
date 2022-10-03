fa97b6bf9ca5884532bd33aded853bdc93ff7683
Luciana Monteiro Cedro <luciana_cedro@hotmail.com>
2022-10-03 18:51:31 -0300

    feature/splash-screen

--- lib/presenter/splash/splash_page.dart
+++ lib/presenter/splash/splash_page.dart
@@ -1,15 +1,50 @@
+import 'dart:io';
+
+import 'package:doginfo/presenter/login/login_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const LoginPage()));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Dog info'),
+      backgroundColor: Colors.amber,
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Image.asset(
+            'assets/images/screen.png',
+            height: 300,
+            width: 400,
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          if (Platform.isIOS)
+            const CupertinoActivityIndicator(
+              radius: 15,
+            )
+          else
+            const CircularProgressIndicator(
+              color: Colors.white,
+            ),
+        ]),
       ),
-      body: Container(),
     );
   }
 }
