5cf63d50f115f70c74898f462199628aeb9efb80
Luciana Monteiro Cedro <luciana_cedro@hotmail.com>
2022-10-14 17:34:08 -0300

    fix/alterando-rota

--- lib/presenter/splash/splash_page.dart
+++ lib/presenter/splash/splash_page.dart
@@ -16,6 +16,6 @@ class SplashPage extends StatelessWidget {
         splashIconSize: 650,
         pageTransitionType: PageTransitionType.rightToLeftWithFade,
         backgroundColor: Colors.amber,
-        nextScreen: HomePage());
+        nextScreen: const LoginPage());
   }
 }
